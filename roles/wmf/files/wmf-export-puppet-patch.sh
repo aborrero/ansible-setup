762f438f5b0ee1a7986270cf0405e841bb01552a
Arturo Borrero Gonzalez <arturo@debian.org>
2022-09-28 19:05:23 +0200

    roles/wmf: install the wmf-export-puppet-patch.sh script
    A script I use to livehack puppetmasters @ WMF.
    
    Signed-off-by: Arturo Borrero Gonzalez <arturo@debian.org>

---
+++ roles/wmf/files/wmf-export-puppet-patch.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+PUPPETMASTER="$1"
+if [ -z "$PUPPETMASTER" ] ; then
+	echo "E: no puppetmaster specified" >&2
+	exit 1
+fi
+
+if [ "$PUPPETMASTER" == "tools" ] ; then
+    PUPPETMASTER="tools-puppetmaster-02.eqiad.wmflabs"
+fi
+if [ "$PUPPETMASTER" == "toolsbeta" ] ; then
+    PUPPETMASTER="toolsbeta-puppetmaster-04.eqiad.wmflabs"
+fi
+if [ "$PUPPETMASTER" == "paws" ] ; then
+    PUPPETMASTER="paws-puppetmaster-01.eqiad.wmflabs"
+fi
+
+PATCH="wmf-export-puppet.patch"
+PATCH_PATH="../${PATCH}"
+git diff origin/HEAD > ${PATCH_PATH}
+scp ${PATCH_PATH} ${PUPPETMASTER}:
+SSH_COMMAND="{ cd /var/lib/git/operations/puppet ;
+		sudo git checkout -f ;
+		sudo git clean -fd ;
+		sudo git pull --rebase ;
+		sudo patch -p1 <~/${PATCH} ;}"
+ssh $PUPPETMASTER $SSH_COMMAND
