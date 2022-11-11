e07e1c9040bbd5306f2fd06ff59c80cda08b1ed3
Arturo Borrero Gonzalez <arturo.bg@arturo.bg>
2022-11-11 18:31:39 +0100

    ansible-setup: enable passwordless become
    This patch introduces a change so there is a way to run a passwordless ansible.
    
    This consists on:
     * creating a system user `ansible-setup`
     * having a sudoers policy for it that means it can run sudo passwordless
     * configure the run.sh script to use this user
    
    Signed-off-by: Arturo Borrero Gonzalez <arturo.bg@arturo.bg>

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-ansible-playbook playbooks/laptop-work.yml -KD
+sudo -u ansible-setup ansible-playbook playbooks/laptop-work.yml -D
+
+# if this is the first run (ie, no ansible-setup user has been created) you will need instead:
+# ansible-playbook playbooks/laptop-work.yml -KD
