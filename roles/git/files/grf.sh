f00628fbc163e49e2b29345f6233aad63a51871e
Arturo Borrero Gonzalez <arturo.bg@arturo.bg>
2023-02-21 14:18:55 +0100

    git: introduce grf script
    New script.
    
    Signed-off-by: Arturo Borrero Gonzalez <arturo.bg@arturo.bg>

---
+++ roles/git/files/grf.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# git refresh current patch, in either stgit or raw commit mode
+
+set -e
+
+if ! ls .git/config >/dev/null 2>&1 ; then
+    echo "E: missing .git/config file, is this a git repo?" >&2
+    exit 1
+fi
+
+if [ "$(git status -s | wc -l)" == "0" ] ; then
+    echo "W: no uncommited changes, do some edit first" >&2
+    exit 1
+fi
+
+if [ "$(git rev-list --count '@{upstream}...HEAD')" == "0" ] ; then
+    echo "W: no commits to refresh, doing nothing" >&2
+    exit 1
+fi
+
+branch=$(git branch --show-current)
+if git branch -l | grep -q "$branch".stgit && [ "$(stg top 2>/dev/null | wc -l)" == "1" ] ; then
+    # stgit context!
+    stg refresh
+    exit $?
+else
+    # raw commit context!
+    git add -A
+    git commit --amend --no-edit
+    exit $?
+fi
