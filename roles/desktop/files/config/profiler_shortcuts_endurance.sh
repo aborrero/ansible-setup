caf083b72120a845c4450a39d8e238207669e666
Arturo Borrero Gonzalez <arturo.bg@arturo.bg>
2023-02-24 22:56:13 +0100

    desktop: endurance: introduce profiler action for restoring screen after suspend
    This bug has been hauting me for years in this laptop.
    
    See https://bugs.debian.org/991788 for more details.
    
    Thanks Uwe Laverenz <uwe@laverenz.de> for the hints.
    
    Signed-off-by: Arturo Borrero Gonzalez <arturo.bg@arturo.bg>

--- roles/desktop/files/config/profiler_shortcuts_endurance.sh
+++ roles/desktop/files/config/profiler_shortcuts_endurance.sh
@@ -7,3 +7,7 @@ function profiler_pulseaudio_hdmi {
 function profiler_pulseaudio_analog {
     profiler exec pulseaudio_analog
 }
+
+function profiler_screen_post_suspend {
+    profiler exec screen_post_suspend
+}
