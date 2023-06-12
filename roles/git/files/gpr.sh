b3213e41c004be4a32ec567e2b10bd90b4683ba4
Arturo Borrero Gonzalez <arturo.bg@arturo.bg>
2023-06-12 12:23:28 +0200

    git: add gpr.sh script
    This script is to wrap common operations I do when pulling.
    
    Signed-off-by: Arturo Borrero Gonzalez <arturo.bg@arturo.bg>

---
+++ roles/git/files/gpr.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+# This script runs git pull --rebase, with support for stgit context
+
+if ! ls .git/config >/dev/null 2>&1 ; then
+    echo "E: missing .git/config file, is this a git repo?" >&2
+    exit 1
+fi
+
+n_stgit_patches="$(stg series -c -A 2>/dev/null || echo 0)"
+stg pop -a 2>/dev/null || true
+
+git pull --rebase
+
+# redo stgit patches, if any
+if [ "${n_stgit_patches}" != "0" ] ; then
+    stg push -m --number="${n_stgit_patches}"
+fi
