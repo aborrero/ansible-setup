01690a8a717b51e29ff98227c1980a34dfd2550a
Arturo Borrero Gonzalez <arturo.bg@arturo.bg>
2024-06-19 12:01:53 +0200

    wmf-cleanup-puppetmaster.sh: refresh script
    Use modern puppetservers and git commands.
    
    Signed-off-by: Arturo Borrero Gonzalez <arturo.bg@arturo.bg>

--- roles/wmf/files/wmf-cleanup-puppetmaster.sh
+++ roles/wmf/files/wmf-cleanup-puppetmaster.sh
@@ -7,19 +7,16 @@ if [ -z "$PUPPETMASTER" ] ; then
 fi
 
 if [ "$PUPPETMASTER" == "tools" ] ; then
-    PUPPETMASTER="tools-puppetmaster-02.tools.eqiad1.wikimedia.cloud"
+    PUPPETMASTER="tools-puppetserver-01.tools.eqiad1.wikimedia.cloud"
 fi
 if [ "$PUPPETMASTER" == "toolsbeta" ] ; then
-    PUPPETMASTER="toolsbeta-puppetmaster-04.toolsbeta.eqiad1.wikimedia.cloud"
-fi
-if [ "$PUPPETMASTER" == "paws" ] ; then
-    PUPPETMASTER="paws-puppetmaster-01.paws.eqiad1.wikimedia.cloud"
+    PUPPETMASTER="toolsbeta-puppetserver-1.toolsbeta.eqiad1.wikimedia.cloud"
 fi
 
-SSH_COMMAND="{ cd /var/lib/git/operations/puppet ;
+SSH_COMMAND="{ cd /srv/git/operations/puppet/ ;
 		sudo git checkout -f ;
 		sudo git clean -fd ;
-		sudo git-sync-upstream ;}"
+		sudo systemctl start puppet-git-sync-upstream.service ;}"
 echo "INFO: cleaning up $PUPPETMASTER"
 echo
 ssh $PUPPETMASTER $SSH_COMMAND
