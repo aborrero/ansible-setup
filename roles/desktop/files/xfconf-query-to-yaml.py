54ae804423a9584c8fddd5c64dbe4d5f22f257b6
Arturo Borrero Gonzalez <arturo.bg@arturo.bg>
2024-08-26 13:45:24 +0200

    desktop: add xfconf-query-to-yaml.py script
    Script to dump a xfconf tree into YAML.
    
    Signed-off-by: Arturo Borrero Gonzalez <arturo.bg@arturo.bg>

---
+++ roles/desktop/files/xfconf-query-to-yaml.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import sys
+import yaml
+import subprocess
+import re
+
+def show_help():
+    print(f"Usage: {sys.argv[0]} [-h] <channel>")
+
+def error(msg: str):
+    print(f"ERROR: {msg}")
+    exit(1)
+
+def run(command: str):
+    result = subprocess.run(command.split(' '), capture_output=True, text=True)
+    return result.stdout.splitlines()
+
+def get_channel_items(channel: str):
+    return run(f"xfconf-query -c {channel} -l")
+
+def translate_item(channel: str, item: str):
+    value = run(f"xfconf-query -c {channel} -p {item}")[0]
+    element = {
+        "channel": channel,
+        "property": item,
+        "value": value,
+        "type": get_type(value),
+        # TODO: force_array?
+        "force_array": "false"
+    }
+
+    return element
+
+def get_type(value: str) -> str:
+    if value == "true" or value == "false":
+        return "bool"
+    if value.isdigit():
+        return "int"
+    if re.match(r"^\d+(\.\d+)?$", value):
+        return "double"
+    return "string"
+
+def main():
+    args = sys.argv[1:]
+
+    if args[0] in ["--help", "-h", "-H"]:
+        show_help()
+        exit(0)
+
+    channel = args[0]
+    channel_items = get_channel_items(channel)
+    if len(channel_items) == 0:
+        error("channel has no items")
+
+    elements = []
+    for item in channel_items:
+        elements.append(translate_item(channel, item))
+
+    print(yaml.safe_dump({"xfce_xfconf_data": elements}))
+
+main()
