8c68602a0e12d9dbeccf5512bfee8e8b58e525cb
Arturo Borrero Gonzalez <arturo.bg@arturo.bg>
2025-04-11 17:04:45 +0200

    desktop: config: add profiler files for xwing
    Just copy from the nostromo for now.
    
    Signed-off-by: Arturo Borrero Gonzalez <arturo.bg@arturo.bg>

---
+++ roles/desktop/files/config/profiler_shortcuts_xwing.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function profiler_pulseaudio_hdmi {
+    profiler exec pulseaudio_hdmi
+}
+
+function profiler_pulseaudio_analog {
+    profiler exec pulseaudio_analog
+}
+
+function profiler_screen_post_suspend {
+    profiler exec screen_post_suspend
+}
